519abcf3068ba0a52554f35054ad91eea0c55557
fpikus <39199330+fpikus@users.noreply.github.com>
2022-01-17 09:15:31 -0800

    Update 11_compare_mbm_c.C

--- Chapter02/11_compare_mbm_c.C
+++ Chapter02/11_compare_mbm_c.C
@@ -1,1 +1,16 @@
+// Microbenchmarks for string comparison using Google benchmark
+#include <cstdlib>
+#include <cstring>
+#include <memory>
 
+#include "benchmark/benchmark.h"
+
+bool compare_uint_l(const char* s1, const char* s2, unsigned int l) {
+    if (s1 == s2) return false;
+    char c1, c2;
+    for (unsigned int i1 = 0, i2 = 0; i1 < l; ++i1, ++i2) {
+        c1 = s1[i1]; c2 = s2[i2];
+        if (c1 != c2) return c1 > c2;
+    }
+    return false;
+}
